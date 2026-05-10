25da68b3c53bfe482eaa5e0a6fda906403ede66f
Abhinav Jaiswal <abhinavj4321@gmail.com>
2026-05-10 07:43:23 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let userInput=document.getElementById("date");
+let submitBtn=document.getElementById("calculate");
+
+userInput.max=new Date().toISOString().split("T")[0];
+
+function calculateAge(){
+    let birthDate=new Date(userInput.value);
+    let birthYear=birthDate.getFullYear();
+    let birthMonth=birthDate.getMonth()+1;
+    let birthDay=birthDate.getDate();
+
+    let currentDate=new Date();
+    let currentYear=currentDate.getFullYear();
+    let currentMonth=currentDate.getMonth()+1;
+    let currentDay=currentDate.getDate();
+
+    let ageDifference,monthDifference,dayDifference;
+    
+    ageDifference=currentYear-birthYear;
+
+    if(currentMonth>=birthMonth){
+        monthDifference=currentMonth-birthMonth;
+    }else{
+        ageDifference--;
+        monthDifference=12+currentMonth-birthMonth;
+    }
+    if(currentDay>=birthDay){
+        dayDifference=currentDay-birthDay;
+    }else{
+        monthDifference--;
+        let daysInPreviousMonth=new Date(currentYear,currentMonth-1,0).getDate();
+        dayDifference=daysInPreviousMonth+currentDay-birthDay;
+    }
+    if(monthDifference<0){
+        monthDifference+=12;
+        ageDifference--;
+    }
+    document.getElementById('result').innerHTML=`Your are <span> ${ageDifference} </span> years, <span> ${monthDifference} </span> and <span> ${dayDifference} </span> days old.`
+}
